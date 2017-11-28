2e7ef2e86eddeaed10b96fabac8e17d6533463fb
Reilly kennon <reilly@Reillys-MBP.domain>
2017-11-27 22:37:06 -0800

    mediating on the meaning of web development

--- creed.js
+++ creed.js
@@ -1,12 +1,42 @@
 // RESTful Poetics
 
-// I make imaginary buildings out of words
-// I command machines to sing 
+
+// RESTful Poetics
+
+// I make imaginary 
+buildings // out of words
+// I 
+command
+// machines to sing 
 // My tool is a glowing box.
 // My fingers invoke electric hammer to nails
-// The command-line-interface is my journal 
+// The 
+command-line-interface 
+//is my journal 
 // The applications are my pages. 
+// My buildings sit atop mountains
+// mostly buried beneath the cyber sea
 
+// Surfers land on my website's beach
+// and humbly make a GET request to data
+// Hidden beneach the waves in the mountains database 
+
+
+
+// I make imaginary 
+const building = {}
+buildings // out of
+{words} // precisely chosen to
+command // machines to sing 
+// My tools rest in a glowing box.
+// I scavenge the web for tools and maertials
+// to bring back to the building
+// My fingers invoke electric hammer to nails
+// The 
+command-line-interface 
+//is my workbook
+// The modules are dexported pages
+// A chapter holds an array of pages
 // My buildings sit atop mountains
 // mostly buried beneath the cyber sea
 
