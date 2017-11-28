947069a9dc678b211c009f2a4e37ed57e4901b0e
Reilly kennon <reilly@Reillys-MBP.domain>
2017-11-27 22:15:42 -0800

    added a poem inspired by this project

---
+++ creed.js
@@ -0,0 +1,15 @@
+// RESTful Poetics
+
+// I make imaginary buildings out of words
+// I command machines to sing 
+// My tool is a glowing box.
+// My fingers invoke electric hammer to nails
+// The command-line-interface is my journal 
+// The applications are my pages. 
+
+// My buildings sit atop mountains
+// mostly buried beneath the cyber sea
+
+// Surfers land on my website's beach
+// and humbly make a GET request to data
+// Hidden beneach the waves in the mountains database 
